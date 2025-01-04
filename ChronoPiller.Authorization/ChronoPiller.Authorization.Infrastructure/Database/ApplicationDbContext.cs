43c409909f073c2cc540590a341489f37e1cc946
lbiel <l.bielenin@gmail.com>
2025-01-04 13:47:05 +0100

    Update refsx

---
+++ ChronoPiller.Authorization/ChronoPiller.Authorization.Infrastructure/Database/ApplicationDbContext.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using ChronoPiller.Authorization.Core.Entities;
+using ChronoPiller.Authorization.Infrastructure.Settings;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace ChronoPiller.Authorization.Infrastructure.Database;
+
+public class ApplicationDbContext(IOptions<DatabaseOptions> options) : DbContext
+{
+    private readonly DatabaseOptions _databaseOptions = options.Value;
+    public DbSet<User> Users { get; set; }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(_databaseOptions.ChronoPiller, options => options.EnableRetryOnFailure());
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<User>()
+            .Property(us => us.Email)
+            .HasMaxLength(50);
+        
+        builder.Entity<User>()
+            .Property(us => us.Name)
+            .HasMaxLength(50);
+        
+        builder.Entity<User>()
+            .Property(us => us.Surname)
+            .HasMaxLength(50);  
+        
+        builder.Entity<User>()
+            .Property(us => us.UserName)
+            .HasMaxLength(50);
+        
+        builder.Entity<User>()
+            .HasIndex(u => u.UserName)
+            .IsUnique();
+
+        base.OnModelCreating(builder);
+    }
+}
---
+++ ChronoPiller.Authorization/ChronoPiller.Authorization.Infrastructure/Settings/DatabaseOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace ChronoPiller.Authorization.Infrastructure.Settings;
+
+public class DatabaseOptions
+{
+    public string ChronoPiller { get; set; }
+    public string HangfireConnection { get; set; }
+}
